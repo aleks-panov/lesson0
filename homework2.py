053fd937ebda06e7009941a01fb80eaa6aeea5ba
aleks-panov <aleks_panov@mail.ru>
2024-06-27 14:46:43 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,8 +1,5 @@
-name=('Aleks')
-print('Name: '+name)
-age=44
-print('Age: ',age)
-age=age+1
-print('New age: ',age)
-is_student=True
-print('Is student: ',is_student)
+name1 = 12
+name2 = 1.5
+name3 = 'Python'
+name4 = name2/name1
+print('Курс: '+name3+', всего задач: ',name1,',затрачено часов: ',name2,',среднее время выполнения ',name4,'часа.')
