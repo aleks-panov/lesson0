3279be36da1b605c254c4fd44091e101675b8e61
aleks-panov <aleks_panov@mail.ru>
2024-06-30 11:56:37 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -2,7 +2,7 @@ immutable_var=(1,2,5,'a','b','d')
 print(immutable_var)
 #immutable_var [0]='c'
 # изменить значение элемента нельзя, кортеж неизменяемый
-mutable_list=([1,2,3],4,5,6)
+mutable_list=[1,2,3,4,5,6]
 print(mutable_list)
-mutable_list[0][1]=7
+mutable_list[0]=7
 print(mutable_list)
