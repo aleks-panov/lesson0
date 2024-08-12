8efae34a30ff4a00be087d1ce0e92352dec59b39
aleks-panov <aleks_panov@mail.ru>
2024-08-12 19:21:04 +0300

    Add files via upload

---
+++ module 5.1.py
@@ -0,0 +1,21 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        num = self.number_of_floors
+        new = list(range(1, new_floor + 1))
+        for i in range(len(new)):
+            if new[i] >= 1 and new[i] <= num:
+                print(new[i])
+            else:
+                print("Такого этажа не существует")
+                break
+
+
+h1 = House("ЖК Горький", 18)
+h2 = House("Домик в деревне", 2)
+h1.go_to(5)
+h2.go_to(10)
