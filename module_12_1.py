0377b3c34eca531f656d773d731c865b17c9b014
aleks-panov <aleks_panov@mail.ru>
2024-11-06 19:25:36 +0300

    Add files via upload

--- module_12_1.py
+++ module_12_1.py
@@ -1,22 +1,34 @@
-import Runner
 import unittest
 
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
 
 class RunnerTest (unittest.TestCase):
     def test_walk(self):
-        runner1 = Runner.Runner('Alex')
+        runner1 = Runner('Alex')
         for walk in range(10):
             runner1.walk()
         self.assertEqual(runner1.distance, 50)
 
     def test_run(self):
-        run1 = Runner.Runner('Alex')
+        run1 = Runner('Alex')
         for run in range(10):
             run1.run()
         self.assertEqual(run1.distance, 100)
     def test_challenge(self):
-        runner1 = Runner.Runner('Alex')
-        run1 = Runner.Runner('Alex')
+        runner1 = Runner('Alex')
+        run1 = Runner('Alex')
         for walk in range(10):
             runner1.walk()
         for run in range(10):
