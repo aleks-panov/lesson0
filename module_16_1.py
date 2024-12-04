d20cd837e52dc1dcbb1ff6d0dfbb3d86551554a4
aleks-panov <aleks_panov@mail.ru>
2024-12-04 17:23:06 +0300

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -4,20 +4,20 @@ app = FastAPI()
 
 
 @app.get('/')
-async def get_main_page():
+async def get_main_page() -> str:
     return "Главная страница"
 
 
 @app.get("/user/admin")
-async def get_admin_page():
+async def get_admin_page() -> str:
     return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def get_username_number(user_id):
+async def get_username_number(user_id) -> dict:
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
 @app.get("/user")
-async def get_user_info(username, age):
+async def get_user_info(username, age) -> set[str]:
     return {f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
