4659d33c52638b8b032c1db0d18d5e62e8120e9d
aleks-panov <aleks_panov@mail.ru>
2024-12-08 12:27:41 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -13,7 +13,7 @@ async def get_users() -> dict:
 
 
 @app.post("/user/{username}/{age}")
-async def post_user(username: Annotated[str, Path(ge=5, le=20, description="Enter username", example="Urban")],
+async def post_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="Urban")],
                     age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="18")]) -> str:
     current_index = str(int(max(users, key=int))+1)
     message = f"Имя: {username}, возраст: {age}"
@@ -22,15 +22,14 @@ async def post_user(username: Annotated[str, Path(ge=5, le=20, description="Ente
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: Annotated[int, Path(ge=1, description="Inter user ID", example="1")],
-                      username: Annotated[str, Path(ge=5, le=20, description="Enter username", example="Urban")],
-                      age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="18")]) -> str:
-    message = f"Имя: {username}, возраст: {age}"
-    users[user_id: str] = message
-    return f"The user {user_id} is updated"
+async def update_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="Urban")],
+                      user_id: int = Path(ge=1, description="Inter user ID", example="1"),
+                      age: int = Path(ge=18, le=120, description="Enter age", example="18")) -> dict:
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return {"message": f"The user {user_id} is updated"}
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: Annotated[int, Path(ge=1, description="Inter user ID", example="1")]) -> str:
+async def delete_user(user_id: str = Path(min_length=1, description="Inter user ID", example="1")) -> dict:
     del users[user_id]
-    return f"User {user_id} has been deleted"
+    return {"message": f"User {user_id} has been deleted"}
