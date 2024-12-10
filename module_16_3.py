4549c9132b1ff20d988a27e30d1e56b0f66f6019
aleks-panov <aleks_panov@mail.ru>
2024-12-10 18:39:23 +0300

    Add files via upload

--- module_16_3.py
+++ module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from typing import Annotated
 
 app = FastAPI()
@@ -25,11 +25,16 @@ async def post_user(username: Annotated[str, Path(min_length=5, max_length=20, d
 async def update_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="Urban")],
                       user_id: int = Path(ge=1, description="Inter user ID", example="1"),
                       age: int = Path(ge=18, le=120, description="Enter age", example="18")) -> dict:
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return {"message": f"The user {user_id} is updated"}
-
+    if user_id in users:
+        users[user_id] = f"Имя: {username}, возраст: {age}"
+        return {"message": f"The user {user_id} is updated"}
+    else:
+        raise HTTPException(status_code=404, detail="User was not found, code 404")
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: str = Path(min_length=1, description="Inter user ID", example="1")) -> dict:
-    del users[user_id]
-    return {"message": f"User {user_id} has been deleted"}
+    if user_id in users:
+        users.pop(user_id)
+        return {"message": f"Пользователь с ID {user_id} удален."}
+    else:
+        raise HTTPException(status_code=404, detail="User was not found, code 404")
