431453234a46e6bda24eecd598e2fd6ded32634b
aleks-panov <aleks_panov@mail.ru>
2024-12-19 15:28:41 +0300

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+
+
+class CreateUser(BaseModel):
+    username: str
+    firstname: str
+    lastname: str
+    age: int
+
+
+class UpdateUser(BaseModel):
+    firstname: str
+    lastname: str
+    age: int
+
+
+class GreateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
+
+
+class UpdateTask(BaseModel):
+    title: str
+    content: str
+    priority: int
